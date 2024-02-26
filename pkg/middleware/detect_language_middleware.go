5c5fa784583b6976f9d5fd26736eb5934d18cf92
Vinicius Okamoto <leo_kamoto@hotmail.com>
2024-02-26 14:57:07 -0600

    feat: add language detection middleware

---
+++ pkg/middleware/detect_language_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/strivesolutions/go-gin-framework/pkg/api"
+	"github.com/strivesolutions/logger-go/pkg/logging"
+)
+
+// DetectLanguage middleware sets the locale code in the request context
+// from the Accept-Language header.
+// Making it easier to share the locale code across the application.
+
+// supported locales
+var supportedLocales = map[string]bool{
+	"en": true,
+	"fr": true,
+}
+
+func DetectLanguage(ctx *gin.Context) {
+	value := ctx.GetHeader("Accept-Language")
+
+	if value == "" {
+		value = "en"
+	}
+
+	if _, ok := supportedLocales[value]; !ok {
+		logging.Info("Unsupported laguage code: " + value)
+		value = "en"
+	}
+
+	api.SetLocaleCode(ctx, value)
+	ctx.Next()
+}
---
+++ tests/locale_middleware_test.go
@@ -0,0 +1,97 @@
+package tests
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/strivesolutions/go-gin-framework/pkg/api"
+	"github.com/strivesolutions/go-gin-framework/pkg/server"
+)
+
+func TestLocaleMiddleware_WhenLocaleCodeIsSet_ShouldReturn200(t *testing.T) {
+	s := server.CreateServer(server.Options{
+		NoPlanIdMiddleware:        true,
+		NoIntegrationIdMiddleware: true,
+		HealthChecks:              passingHealthChecks(),
+	})
+
+	s.AddRoute(api.ApiRoute{
+		MethodType: api.GET,
+		Anonymous:  true,
+		Path:       "/",
+		Handler: func(ctx *gin.Context) {
+			localeCode := api.GetLocaleCode(ctx)
+
+			api.OkResponse(ctx, localeCode)
+		},
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("Accept-Language", "en")
+	w := httptest.NewRecorder()
+	s.Engine.ServeHTTP(w, req)
+
+	body, _ := io.ReadAll(w.Body)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"data":"en","error":null}`, string(body))
+}
+
+func TestLocaleMiddleware_WhenLocaleIsNotInHeader_ShouldReturn200(t *testing.T) {
+	s := server.CreateServer(server.Options{
+		NoPlanIdMiddleware:        true,
+		NoIntegrationIdMiddleware: true,
+		HealthChecks:              passingHealthChecks(),
+	})
+
+	s.AddRoute(api.ApiRoute{
+		MethodType: api.GET,
+		Anonymous:  true,
+		Path:       "/",
+		Handler: func(ctx *gin.Context) {
+			localeCode := api.GetLocaleCode(ctx)
+
+			api.OkResponse(ctx, localeCode)
+		},
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	s.Engine.ServeHTTP(w, req)
+
+	body, _ := io.ReadAll(w.Body)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"data":"en","error":null}`, string(body))
+}
+
+func TestLocaleMiddleware_WhenLocaleIsNotSupported_ShouldReturn200(t *testing.T) {
+	s := server.CreateServer(server.Options{
+		NoPlanIdMiddleware:        true,
+		NoIntegrationIdMiddleware: true,
+		HealthChecks:              passingHealthChecks(),
+	})
+
+	s.AddRoute(api.ApiRoute{
+		MethodType: api.GET,
+		Anonymous:  true,
+		Path:       "/",
+		Handler: func(ctx *gin.Context) {
+			localeCode := api.GetLocaleCode(ctx)
+
+			api.OkResponse(ctx, localeCode)
+		},
+	})
+
+	req, _ := http.NewRequest("GET", "/", nil)
+	req.Header.Add("Accept-Language", "es")
+	w := httptest.NewRecorder()
+	s.Engine.ServeHTTP(w, req)
+
+	body, _ := io.ReadAll(w.Body)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, `{"data":"en","error":null}`, string(body))
+
+}
